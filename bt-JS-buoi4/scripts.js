147a331b5e57ba1034ebbbed6b3b0c673116f5e9
nateriver2204 <73120991+nateriver2204@users.noreply.github.com>
2020-11-23 18:51:53 +0700

    gz

--- bt-JS-buoi4/scripts.js
+++ bt-JS-buoi4/scripts.js
@@ -27,15 +27,32 @@
 // }
 
 //Viết hàm nhận 2 tham số, một là object, 2 là key, nếu key có tồn tại thì trả về giá trị của key đó, nếu không, trả về false. Sau đó, in ra màn hình giá trị trả về, nếu không có in ra “Không có ‘key’”.
-// let user = {};
-// user.name = "Quang Anh";
-// user.age = 23;
-// function oak(a = user, b) {
-//   for (let key in user) {
-//     if (b = key) {
-//       console.log(user[b]);
-//     } else {
-//       console.log("False");
+let user = {};
+user.name = "Quang Anh";
+user.age = 23;
+user.phonenumber = "09787925446";
+// function checkKey(obj, key) {
+//     for(let i in obj){
+//         if (i === key){
+//             console.log(key + ": " + obj[key]);
+// return true;
+//         }
+//     }
+//     console.log("không có key này.");
+//     return false;
+// }
+
+// Viết hàm để xóa một key trong object, hàm sẽ nhận 2 tham số, 1 là object, 2 là key cần xóa, hàm trả về giá trị của key sau khi xóa, hoặc false nếu key không tồn tại. In ra giá trị của key bị xóa, nếu không tồn tại, in ra “Không có key”
+
+// function delKey(obj, key) {
+//   for (let i in obj) {
+//     if (i === key) {
+//       console.log("đã xóa " + key + ": " + obj[key]);
+//       delete obj[key];
+//       console.log(obj);
+//       return true;
 //     }
 //   }
+//   console.log("không có key này.");
+//   return false;
 // }
