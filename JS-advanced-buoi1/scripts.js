ea68762c63092eaa0e4987a6c3509e8665100a16
nateriver2204 <73120991+nateriver2204@users.noreply.github.com>
2021-03-22 20:00:16 +0700

    okeokeoke

--- JS-advanced-buoi1/scripts.js
+++ JS-advanced-buoi1/scripts.js
@@ -7,3 +7,28 @@ $("#back-btn").click(() => {
   $(".studentlist-wrapper").toggle("active");
   $(".addnewstudent-wrapper").toggle("active");
 });
+
+// add info
+$("#save-btn").click(() => {
+  $(".studentlist-wrapper").toggle("active");
+  $(".addnewstudent-wrapper").toggle("active");
+  $.post("http://localhost:3000/users", {
+    name: `${$("#inputName").val()}`,
+    birthday: `${$("#inputYear").val()}`,
+    email: `${$("#inputEmail").val()}`,
+    phone: `${$("#inputPhone").val()}`,
+  });
+});
+// let info = {
+//   name: `${$("#inputName").val()}`,
+//   birthday: `${$("#inputYear").val()}`,
+//   email: `${$("#inputEmail").val()}`,
+//   phone: `${$("#inputPhone").val()}`,
+// };
+
+// $.ajax({
+//   type: "POST",
+//   url: "http://localhost:3000/users",
+//   data: info,
+//   dataType: "",
+// });
--- JS-advanced-buoi2/public/scripts.js
+++ JS-advanced-buoi2/public/scripts.js
@@ -1,7 +1,7 @@
-// $("#add-btn").click(() => {
-//   $(".studentlist-wrapper").toggle("active");
-//   $(".addnewstudent-wrapper").toggle("active");
-// });
+$("#add-btn").click(() => {
+  $(".studentlist-wrapper").toggle("active");
+  $(".addnewstudent-wrapper").toggle("active");
+});
 
 $("#back-btn").click(() => {
   $(".studentlist-wrapper").toggle("active");
@@ -36,31 +36,28 @@ function loadDoc() {
 }
 loadDoc();
 
-function loadOnClick() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      $("#add-btn").click(() => {
-        let x = Math.floor(Math.random() * 11 + 1);
-        $("tbody").append(`<tr>
-              <th scope="row">${JSON.parse(this.responseText)[x].name}</th>
-              <td>${JSON.parse(this.responseText)[x].birthday}</td>
-              <td>${JSON.parse(this.responseText)[x].email}</td>
-              <td>${JSON.parse(this.responseText)[x].phone}</td>
-              <td>
-                <span class="edit-btn"
-                  ><i class="fas fa-edit"></i> Chỉnh sửa
-                </span>
-                <span>|</span>
-                <span class="trash-btn"
-                  ><i class="fas fa-trash-alt"></i> Xóa</span
-                >
-              </td>
-            </tr>`);
-      });
-    }
-  };
-  xhttp.open("GET", "http://localhost:3000/users", true);
-  xhttp.send();
-}
-loadOnClick();
+// add info
+$("#save-btn").click(() => {
+  $(".studentlist-wrapper").toggle("active");
+  $(".addnewstudent-wrapper").toggle("active");
+  $.post("http://localhost:3000/users", {
+    name: `${$("#inputName").val()}`,
+    birthday: `${$("#inputYear").val()}`,
+    email: `${$("#inputEmail").val()}`,
+    phone: `${$("#inputPhone").val()}`,
+  });
+  loadDoc();
+});
+// let info = {
+//   name: `${$("#inputName").val()}`,
+//   birthday: `${$("#inputYear").val()}`,
+//   email: `${$("#inputEmail").val()}`,
+//   phone: `${$("#inputPhone").val()}`,
+// };
+
+// $.ajax({
+//   type: "POST",
+//   url: "http://localhost:3000/users",
+//   data: info,
+//   dataType: "",
+// });
