0bd59a3da6c63b2e5f944cb4279159ca3ffb4067
nateriver2204 <73120991+nateriver2204@users.noreply.github.com>
2021-03-25 16:44:37 +0700

    okeokeoke

--- JS-advanced-buoi3/scripts.js
+++ JS-advanced-buoi3/scripts.js
@@ -2,24 +2,43 @@ function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      for (i = 0; i < JSON.parse(this.responseText).length; i++) {
+      let users = JSON.parse(this.responseText);
+      for (i = 0; i < users.length; i++) {
         $("tbody").append(`<tr>
-              <th scope="row">${JSON.parse(this.responseText)[i].name}</th>
-              <td>${JSON.parse(this.responseText)[i].birthday}</td>
-              <td>${JSON.parse(this.responseText)[i].email}</td>
-              <td>${JSON.parse(this.responseText)[i].phone}</td>
+              <th scope="row">${users[i].name}</th>
+              <td>${users[i].birthday}</td>
+              <td>${users[i].email}</td>
+              <td>${users[i].phone}</td>
               <td>
-                <a class="edit-btn"
+                <a class="edit-btn" href="edit.html?${users[i].id}"
                   ><i class="fas fa-edit"></i> Chỉnh sửa
                 </a>
                 <span>|</span>
-                <a class="trash-btn"
+                <a class="trash-btn" data-id="${users[i].id}"
                   ><i class="fas fa-trash-alt"></i> Xóa</a
-                >
+                >                
               </td>
+              <td><input type="checkbox" class="del-checkbox" data-del-id="${users[i].id}"></input></td>              
             </tr>`);
       }
     }
+    // del info
+    $(".trash-btn").click(function () {
+      let delId = $(this).attr("data-id");
+      $(".delete-noti-wrapper").addClass("active");
+      $("#not-del-btn").click(() => {
+        $(".delete-noti-wrapper").removeClass("active");
+      });
+      $("#del-btn").click(() => {
+        $(".delete-noti-wrapper").removeClass("active");
+        $.ajax({
+          type: "DELETE",
+          url: "https://nateriver2204.herokuapp.com/users/" + delId,
+        });
+        $(this).parent().parent().remove();
+      });
+    });
+    //
   };
   xhttp.open("GET", "https://nateriver2204.herokuapp.com/users", true);
   xhttp.send();
@@ -28,13 +47,16 @@ loadDoc();
 
 // add info
 $("#save-btn").click(() => {
-  $(".studentlist-wrapper").toggle("active");
-  $(".addnewstudent-wrapper").toggle("active");
   $.post("https://nateriver2204.herokuapp.com/users", {
-    name: `${$("#inputName").val()}`,
-    birthday: `${$("#inputYear").val()}`,
-    email: `${$("#inputEmail").val()}`,
-    phone: `${$("#inputPhone").val()}`,
+    name: $("#inputName").val(),
+    birthday: $("#inputYear").val(),
+    email: $("#inputEmail").val(),
+    phone: $("#inputPhone").val(),
   });
-  loadDoc();
 });
+// onclick="removeUser(${users[i].id}, this)" cho moi button
+$("#check-all").click(() => {
+  $(".del-checkbox").click();
+});
+
+// del function
