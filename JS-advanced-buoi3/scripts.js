ce4fb590b71729ce725d01550276846cd5ece217
nateriver2204 <73120991+nateriver2204@users.noreply.github.com>
2021-04-02 18:45:21 +0700

    okk

--- JS-advanced-buoi3/scripts.js
+++ JS-advanced-buoi3/scripts.js
@@ -1,28 +1,35 @@
-function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      let users = JSON.parse(this.responseText);
-      for (i = 0; i < users.length; i++) {
-        $("tbody").append(`<tr>
-              <th scope="row">${users[i].name}</th>
-              <td>${users[i].birthday}</td>
-              <td>${users[i].email}</td>
-              <td>${users[i].phone}</td>
-              <td>
-                <a class="edit-btn" href="edit.html?${users[i].id}"
-                  ><i class="fas fa-edit"></i> Chỉnh sửa
-                </a>
-                <span>|</span>
-                <a class="trash-btn" data-id="${users[i].id}"
-                  ><i class="fas fa-trash-alt"></i> Xóa</a
-                >                
-              </td>
-              <td><input type="checkbox" class="del-checkbox" data-del-id="${users[i].id}" onclick="getId(${users[i].id})"></input></td>              
-            </tr>`);
+let content = "";
+let numberOfId = 0;
+let page = 1;
+let pagelimit = 5;
+function getData() {
+  $.get(
+    "https://nateriver2204.herokuapp.com/users?_sort=id&_order=desc" +
+      "_page=" +
+      page +
+      "&_limit=" +
+      pagelimit,
+    function appendData(users) {
+      for (user of users) {
+        content += `<tr>
+                   <th>${user.name}</th>
+                   <td>${user.birthday}</td>
+                   <td>${user.email}</td>
+                   <td>${user.phone}</td>
+                   <td>
+                      <a class="edit-btn" href="edit.html?${user.id}"><i class="fas fa-edit"></i> Chỉnh sửa</a>
+                      <span>|</span>
+                      <a class="trash-btn" data-id="${user.id}"><i class="fas fa-trash-alt"></i> Xóa</a>
+                   </td>
+                   <td><input type="checkbox" class="del-checkbox" data-del-id="${user.id}" ></input></td>
+               </tr>`;
       }
+      console.log(users);
     }
-    // del info trash bin
+  ).done(function () {
+    //append data to html
+    $("tbody").html(content);
+    //function del-btn
     $(".trash-btn").click(function () {
       let delId = $(this).attr("data-id");
       $(".delete-noti-wrapper").addClass("active");
@@ -40,43 +47,35 @@ function loadDoc() {
           $(this).parent().parent().remove();
         });
     });
-    // del info checkbox
+    // function del-all checkbox:checked
 
-    // $(".del-checkbox").click(function () {
-    //   listId.push($(this).attr("data-del-id"));
-    //   console.log(listId);
-
-    //     $("#del-all").click(() => {
-    //       if ($(":checked").length > 0) {
-    //         $(".delete-noti-wrapper").addClass("active");
-    //         $("#not-del-btn").click(() => {
-    //           $(".delete-noti-wrapper").removeClass("active");
-    //         });
-    //         $("#del-btn")
-    //           .off("click")
-    //           .click(() => {
-    //             $(".delete-noti-wrapper").removeClass("active");
-    //             $.ajax({
-    //               type: "DELETE",
-    //               url: "https://nateriver2204.herokuapp.com/users/" + delId,
-    //             });
-
-    //             $(this).parent().parent().remove();
-    //           });
-    //       }
-    //     });
-    // });
-  };
-  xhttp.open(
-    "GET",
-    "https://nateriver2204.herokuapp.com/users?_sort=id&_order=desc",
-    true
-  );
-  xhttp.send();
+    $("#del-all").click(function () {
+      let checkId = [];
+      if ($(".del-checkbox:checked").length > 0) {
+        $(".del-checkbox:checked").each(function () {
+          checkId.push(+$(this).attr("data-del-id"));
+          $("#del-btn").click(() => {
+            for (let i = 0; i < checkId.length; i++) {
+              $.ajax({
+                type: "DELETE",
+                url: "https://nateriver2204.herokuapp.com/users/" + checkId[i],
+              });
+            }
+            $(this).parent().parent().remove();
+            $(".delete-noti-wrapper").removeClass("active");
+          });
+        });
+        $(".delete-noti-wrapper").addClass("active");
+        $("#not-del-btn").click(() => {
+          $("#del-btn").off("click");
+          $(".delete-noti-wrapper").removeClass("active");
+        });
+      } else return;
+    });
+    // get number of id
+    console.log(numberOfId);
+  });
 }
-loadDoc();
-
-// add info
 
 // onclick="removeUser(${users[i].id}, this)" cho moi button
 let countClick = 0;
@@ -88,15 +87,18 @@ $("#check-all").click(() => {
     $(".del-checkbox").prop("checked", false);
   }
 });
-// show id function
-let listId = [];
-
-function getId(id) {
-  listId.push(id);
-  console.log(listId);
-}
-$("#del-all").click(() => {
-  for (i = 0; i < listId.length; i++) {
-    console.log($("[data-del-id = i]"));
+// prev next page
+$("#prev-btn").click(function () {
+  if (page > 1) {
+    page--;
+    console.log(page);
+    getData();
+  }
+});
+$("#next-btn").click(function () {
+  if (page * pagelimit < numberOfId) {
+    page++;
+    console.log(page);
+    getData();
   }
 });
